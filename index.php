723b4dee72866e2ca3b7bd74211cb6861fed5a61
Sarah <leawine@gmail.com>
2021-01-25 15:06:04 +0100

    V/1.2. Git fait depuis la version locale et non le serveur linux

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+//mise en buffer
+ob_start();
+$action = filter_input(INPUT_GET, 'action', FILTER_SANITIZE_URL);
+
+//tableau de routes
+$routes = [
+
+];
+
+//Condition qui détermine le contenu à afficher en fonction des paramètres $action demandés (ou non)
+if (isset($action)) {
+    if (array_key_exists($action, $routes)) {
+        require $routes[$action];
+    } else {
+        require $routes['404'];
+    }
+} else {
+    require $routes['accueil'];
+}
+//affichage de ce qui était en buffer via la variable $render
+$render = ob_get_clean();
+echo $render;
